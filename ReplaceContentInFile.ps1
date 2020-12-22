bb80e3ebeea57b36384a23cc5427535914ffd251
Jacob273 <jakubgmur@op.pl>
2020-12-22 16:58:02 +0100

    Add files via upload

---
+++ ReplaceContentInFile.ps1
@@ -0,0 +1,14 @@
+﻿$pathToAppConfig1 = "C:\app\App1.exe.config"
+$pathToAppConfig2 = "C:\app2\App2.exe.config"
+
+
+
+(Get-Content -path $pathToAppConfig1 -Raw) -replace 
+'net.tcp://localhost:50002/', 'net.tcp://localhost:63332' | 
+Set-Content -Path $pathToAppConfig1
+
+
+(Get-Content -path $pathToAppConfig2 -Raw) -replace 
+{ 'net.tcp://localhost:50002', 'net.tcp://localhost:63332',
+  'baseAddress="http://localhost:50001', 'baseAddress="http://localhost:63331' } | 
+Set-Content -Path $pathToAppConfig2
